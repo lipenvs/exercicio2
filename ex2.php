65c36a29493959f06d1e45a157d1a264d41f0b90
Felipe Neves <rj.felipeneves@gmail.com>
2024-03-27 00:27:55 -0300

    Remoção do html

--- ex2.php
+++ ex2.php
@@ -1,29 +1,15 @@
-<!DOCTYPE html>
-<html lang="pt-br">
 
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>EXERCÍCIO 2 - DESAFIO DEV - PALÍNDROMO</title>
-</head>
+<?php
 
-<body>
-  <h1>
-    <?php
+function isPalindrome($string)
+{
+  return strtolower($string) === strrev($string);
+}
 
-    function isPalindrome($string)
-    {
-      return strtolower($string) === strrev($string);
-    }
+// Coloque aqui o seu texto (Input)
+$input = "arara";
 
-    // Coloque aqui o seu texto (Input)
-    $input = "arara";
+$output = isPalindrome($input);
+echo "Output: " . ($output ? 'True' : 'False');
 
-    $output = isPalindrome($input);
-    echo "Output: " . ($output ? 'True' : 'False');
-
-    ?>
-  </h1>
-</body>
-
-</html>
+?>
